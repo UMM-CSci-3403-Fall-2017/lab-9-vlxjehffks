a7f9e98afc92adccf119de5bdea6058d276ece07
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-12-05 11:39:07 -0600

    need a lot of works

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,8 +1,23 @@
 package segmentedfilesystem;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) thrwos IOException {
+	    InetAddress address = InetAddress.getByName("localhost");
+
+	    DatagramSocket socket = new DatagramSocket(6014);
+
+	    byte buffer[] = new byte[1024];
+
+	    DatagramPacket buffer = new DtagramPakcet(buffer, buffer.length, address, port);
+
+
+
         
     }
 
