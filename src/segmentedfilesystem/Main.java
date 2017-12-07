bcfb6f0e540a3ab2858cab7c5cb00cd2eac22c3c
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-12-07 02:37:05 -0600

    I don't know

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -8,18 +8,16 @@ import java.util.*;
 public class Main {
     
     public static void main(String[] args) thrwos IOException {
+	    int port = 6014;
+	    byte[] buf = new byte[1024];
 	    InetAddress address = InetAddress.getByName("localhost");
-
 	    DatagramSocket socket = new DatagramSocket(6014);
+	    DatagramPacket packet = new DataPacket(buf, 0, address, 6014);
+	    socket.receive(packet);
 
-	    byte buffer[] = new byte[1024];
-
-	    DatagramPacket packet = new DtagramPakcet(packet, packet.length, address, port);
-	    socket.send(packet);
-
-
-
-
+	    ArrayList<gram> f1 = new ArrayList<gram>();
+	    ArrayList<gram> f2 = new ArrayList<gram>();
+	    ArrayList<gram> f3 = new ArrayList<gram>();
 
 
 
