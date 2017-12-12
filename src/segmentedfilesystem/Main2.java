0e80d40825601acd108429f52015a6da2662f404
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-12-12 17:54:21 -0600

    Changed

---
+++ src/segmentedfilesystem/Main2.java
@@ -0,0 +1,48 @@
+package segmentedfilesystems;
+
+
+import java.util.oi.*;
+import java.net.*;
+
+public class Main2{
+	public static void main(String[] args){
+		int port = 6014;
+	try{
+		InetAddress = address = InetAddress.getByName("localhost");
+		DatagramSocket socket = new DatagramSocket();
+	
+		while (true) {
+			byte[] one = new byte[1];
+			DatagramPacket request = new DatagramPacket(one, 1, address, port);
+			socket.send(request);
+
+			byte[] buffer = new byte[1028];
+			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+			socket.receive(response);
+
+			
+			String quote = new String(buffer, 0, response.getLength());
+			System.out.println(quote);
+			System.out.println();
+
+			Thread.sleep(10000);
+		}
+		catch (SocketTimeoutException ex) {
+			System.out.println("Timeout error: " + ex.getMesseage());
+			ex.printStackTrace();
+		}
+		catch (IOException ex) {
+			System.out.println("Client error: " + ex.getMesseage());
+			ex.printStackTrace();
+		}
+		catch (InterruptedException ex){
+			ex.printStackTrace();
+		}
+	}
+	}
+
+
+
+		
+
+		
