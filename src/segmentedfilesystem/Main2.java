2eec7a17c12afb0d2c98c63dbc6cf9af230844d7
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-12-12 20:08:58 -0600

    pushed

--- src/segmentedfilesystem/Main2.java
+++ src/segmentedfilesystem/Main2.java
@@ -6,6 +6,10 @@ import java.net.*;
 
 public class Main2{
 	public static void main(String[] args){
+		if (args.length < 2) {
+			System.out.println("Syntax: Main <hostname> <port>");
+			return;
+		}
 		int port = 6014;
 	try{
 		InetAddress address = InetAddress.getByName("localhost");
